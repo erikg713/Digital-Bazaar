46545c86fd6b6e457423027af84acfd84a7dd986
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 07:48:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+  port: process.env.PORT || 5000,
+
+  // PostgreSQL connection config
+  pg: {
+    user: process.env.PG_USER || 'postgres',
+    host: process.env.PG_HOST || 'localhost',
+    database: process.env.PG_DATABASE || 'yourdbname',
+    password: process.env.PG_PASSWORD || '',
+    port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+  },
+
+  jwtSecret: process.env.JWT_SECRET || 'defaultsecret',
+  nodeEnv: process.env.NODE_ENV || 'development',
+};
+
+export default config;
 
