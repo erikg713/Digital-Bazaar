1b6f804fe9e2414e916a53b8eeb8fb0e574b9a9c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 07:30:39 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,32 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// Password hashing middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Match password for login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 export const UserModel = {
   createTable: `
     CREATE TABLE IF NOT EXISTS users (
@@ -8,3 +37,7 @@ export const UserModel = {
     );
   `,
 };
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
