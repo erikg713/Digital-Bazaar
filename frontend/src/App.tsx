54353886f4f508e3e830f6e59ecbe7b013bd3a8c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 00:14:55 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import CreatorDashboard from './pages/CreatorDashboard';
+import Marketplace from './pages/Marketplace';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/creator" element={<CreatorDashboard />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
