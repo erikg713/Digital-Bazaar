c5ee46066559eb95033d6da5d81140ea1f380a34
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 07:36:00 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,3 @@
-import pg from 'pg';
-const { Pool } = pg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const connectDB = async () => {
-  try {
-    await pool.query('SELECT NOW()');
-    console.log('PostgreSQL connected');
-  } catch (err) {
-    console.error('PostgreSQL connection error', err);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
-export { pool };
-
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
