159b2a72a9803250ac16f22bb0ff0dbcea9a017a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-01 00:26:47 -0500

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+// backend/config.js
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+};
